55fcdf67ee9e13539311bb7a9044fa9e69f79b4e
Ryan Gamel <ryan.gamel.ext@bayer.com>
2025-12-01 10:25:06 -0600

    day 1a complete

--- day01/main.go
+++ day01/main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello AOC!")
+	lines := []string{}
+	curr := 50
+	count := 0
+
+	// read input file
+	f, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatalf("Problem reading file: %v", err)
+	}
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		next := s.Text()
+		lines = append(lines, next)
+	}
+
+	for _, line := range lines {
+		dir, amt := ParseDirection(line)
+
+		rotFn := RotateRight
+
+		if dir == "L" {
+			rotFn = RotateLeft
+		}
+		curr = rotFn(curr, amt)
+		CheckForZero(curr, &count)
+	}
+
+	fmt.Printf("password: %d\n", count)
+}
+
+func ParseDirection(input string) (dir string, amt int) {
+	dir = string(input[0])
+
+	amt, err := strconv.Atoi((input[1:]))
+	if err != nil {
+		log.Fatalf("could not read line: %s", input)
+	}
+
+	return dir, amt
+}
+
+func RotateRight(start int, dist int) (end int) {
+	end = start + dist
+	if end > 99 {
+		end = end % 100
+	}
+	return end
+}
+
+func RotateLeft(start int, dist int) (end int) {
+	end = start - dist
+	for end < 0 {
+		end = 100 + end
+	}
+	return end
+}
+
+func CheckForZero(input int, count *int) {
+	if input == 0 {
+		*count++
+	}
 }
--- day01/main_test.go
+++ day01/main_test.go
@@ -2,6 +2,63 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	t.Errorf("%s", "not implemented")
+type test struct {
+	in   int
+	dist int
+	out  int
+}
+
+func TestRotateRight(t *testing.T) {
+	tests := []test{
+		{in: 99, dist: 1, out: 0},
+		{in: 0, dist: 1, out: 1},
+		{in: 50, dist: 50, out: 0},
+		{in: 51, dist: 30, out: 81},
+	}
+	for _, tt := range tests {
+		result := RotateRight(tt.in, tt.dist)
+		if result != tt.out {
+			t.Errorf("wanted %d, got %d", tt.out, result)
+		}
+	}
+}
+
+func TestRotateLeft(t *testing.T) {
+	tests := []test{
+		{in: 99, dist: 1, out: 98},
+		{in: 0, dist: 1, out: 99},
+		{in: 50, dist: 50, out: 0},
+		{in: 51, dist: 30, out: 21},
+		{in: 20, dist: 100, out: 20},
+	}
+	for _, tt := range tests {
+		result := RotateLeft(tt.in, tt.dist)
+		if result != tt.out {
+			t.Errorf("wanted %d, got %d", tt.out, result)
+		}
+	}
+}
+
+func TestCheckForZero(t *testing.T) {
+	store := store{count: 0}
+	CheckForZero(1, &store)
+
+	if store.count != 0 {
+		t.Errorf("wanted %d, got %d", 0, store.count)
+	}
+
+}
+
+func TestParseDirection(t *testing.T) {
+	input := "L25"
+
+	dir, amt := ParseDirection(input)
+
+	if dir != "L" {
+		t.Errorf("Going the wrong way")
+	}
+
+	if amt != 25 {
+		t.Errorf("Wrong amt")
+	}
 }
