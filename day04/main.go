a076c300bfb13a5a1fa76c422e49c99ce3c4f737
Ryan Gamel <ryan.gamel.ext@bayer.com>
2025-12-04 11:19:14 -0600

    day04 a&b right answers

--- day04/main.go
+++ day04/main.go
@@ -21,9 +21,72 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		lines = append(lines, line)
+	}
+
+	fmt.Printf("Part A: %d\n", PartA(lines))
+	fmt.Printf("Part B: %d\n", PartB(lines))
+}
+
+func PartA(matrix []string) int {
+	rolls := 0
+	for rowI := range matrix {
+		for colI := range matrix {
+			rolls += ScanAdjacent(rowI, colI, matrix)
+		}
+	}
+	return rolls
+}
+
+func PartB(matrix []string) int {
+	rolls := 0
+	next := matrix
+	for {
+		curr := 0
+		for row := 0; row < len(matrix); row++ {
+			for col := 0; col < len(matrix[row]); col++ {
+				res := ScanAdjacent(row, col, matrix)
+				if res == 1 {
+					rolls++
+					curr++
+					next[row] = matrix[row][:col] + "." + matrix[row][col+1:]
+				}
+			}
+		}
+		if curr == 0 {
+			break
+		}
+		matrix = next
+	}
+	return rolls
+}
+
+func ScanAdjacent(row int, col int, matrix []string) int {
+	if matrix[row][col] == '.' {
+		return 0
+	}
+	adjRolls := 0
+	for direction := range 9 {
+		if direction == 4 {
+			// this is self
+			continue
+		}
+		n_row := row + ((direction % 3) - 1)
+		n_col := col + ((direction / 3) - 1)
+		if n_row >= 0 && n_row < len(matrix) && n_col >= 0 && n_col < len(matrix[0]) {
+			// a valid neighbor
+			if matrix[n_row][n_col] == '@' {
+				adjRolls++
+			}
+			if adjRolls > 3 {
+				return 0
+			}
+		}
+	}
+	return 1
+}
+
+func printMatrix(matrix []string) {
+	for _, line := range matrix {
 		fmt.Println(line)
 	}
 }
-
-func PartA() {}
-func PartB() {}
--- day04/main_test.go
+++ day04/main_test.go
@@ -1,10 +1,72 @@
 package main
 
 import (
+	"bufio"
+	"log"
+	"os"
 	"testing"
 )
 
-func TestStub(t *testing.T) {
-	t.Error("not implemented")
+func getTestMatrix() []string {
+	f, err := os.Open("./testinput.txt")
+	if err != nil {
+		log.Fatalf("problem opening test file: %v", err)
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	matrix := []string{}
+	for scanner.Scan() {
+		matrix = append(matrix, scanner.Text())
+	}
+	return matrix
+}
 
+func TestScanAdjacent(t *testing.T) {
+	tests := []struct {
+		row int
+		col int
+		res int
+	}{
+		{0, 0, 0},
+		{0, 1, 0},
+		{0, 2, 1},
+		{0, 3, 1},
+		{0, 4, 0},
+		{0, 5, 1},
+		{0, 6, 1},
+		{0, 7, 0},
+		{0, 8, 1},
+		{0, 9, 0},
+		{1, 0, 1},
+		{1, 1, 0},
+		{1, 1, 0},
+		{9, 8, 1},
+	}
+	matrix := getTestMatrix()
+
+	for _, tt := range tests {
+		res := ScanAdjacent(tt.row, tt.col, matrix)
+		if tt.res != res {
+			t.Errorf("got %d want %d", res, tt.res)
+
+		}
+	}
+}
+
+func TestPartA(t *testing.T) {
+	matrix := getTestMatrix()
+	res := PartA(matrix)
+
+	if res != 13 {
+		t.Errorf("Got %d, want 13", res)
+	}
+}
+
+func TestPartB(t *testing.T) {
+	matrix := getTestMatrix()
+	res := PartB(matrix)
+
+	if res != 43 {
+		t.Errorf("Got %d, want 43", res)
+	}
 }
