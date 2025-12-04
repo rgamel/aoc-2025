57ac1b73061ceec4443e5731ec8c184c7ca551a9
Ryan Gamel <ryan.gamel.ext@bayer.com>
2025-12-03 20:30:53 -0600

    day 3b right answer

--- day03/main.go
+++ day03/main.go
@@ -16,13 +16,15 @@ func main() {
 	defer f.Close()
 
 	joltage := 0
-
+	maxJoltage := 0
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		joltage += FindLineJoltage(scanner.Text())
+		text := scanner.Text()
+		joltage += FindLineJoltage(text)
+		maxJoltage += FindMaxLineJoltage(text)
 	}
-
 	fmt.Printf("part a: %d\n", joltage)
+	fmt.Printf("part b: %d\n", maxJoltage)
 }
 
 func FindLineJoltage(line string) int {
@@ -41,4 +43,44 @@ func FindLineJoltage(line string) int {
 		}
 	}
 	return result
+}
+
+func FindMaxLineJoltage(line string) int {
+	p1 := 0
+	p2 := len(line) - 12
+	output := ""
+
+	for p2 < len(line) {
+		subStr := line[p1 : p2+1]
+		subMax, idx, _ := findMaxDigit(subStr)
+		output = output + fmt.Sprint(subMax)
+		p1 += idx + 1
+		p2++
+	}
+	parsed, _ := strconv.Atoi(output)
+	return parsed
+}
+
+func findMaxStartIndex(line string, batteryCount int) int {
+	return len(line) - batteryCount
+}
+
+func findMaxDigit(s string) (int, int, error) {
+	maxDigit := 0
+	idx := -1
+	for i, char := range s {
+		if char >= '0' && char <= '9' {
+			digit, err := strconv.Atoi(string(char))
+			if err != nil {
+				return -1, -1, fmt.Errorf("error converting char to digit %w", err)
+			}
+
+			if digit > maxDigit {
+				maxDigit = digit
+				idx = i
+			}
+		}
+	}
+
+	return maxDigit, idx, nil
 }
--- day03/main_test.go
+++ day03/main_test.go
@@ -23,3 +23,44 @@ func TestFindLineJoltage(t *testing.T) {
 	}
 
 }
+
+func TestFindMaxLineJoltage(t *testing.T) {
+	tests := []Test{
+		{in: "987654321111111", out: 987654321111},
+		{in: "811111111111119", out: 811111111119},
+		{in: "234234234234278", out: 434234234278},
+		{in: "818181911112111", out: 888911112111},
+	}
+
+	for _, tt := range tests {
+		res := FindMaxLineJoltage(tt.in)
+		if res != tt.out {
+			t.Errorf("got %d, want %d", res, tt.out)
+		}
+	}
+
+}
+
+func TestFindMaxDigit(t *testing.T) {
+	type MaxTest struct {
+		in  string
+		idx int
+		out int
+	}
+	tests := []MaxTest{
+		{in: "987654321", idx: 0, out: 9},
+		{in: "89654321", idx: 1, out: 9},
+		{in: "12344321", idx: 3, out: 4},
+	}
+
+	for _, tt := range tests {
+		max, i, _ := findMaxDigit(tt.in)
+		if max != tt.out {
+			t.Errorf("wrong max, got %d, want %d", max, tt.out)
+		}
+		if i != tt.idx {
+			t.Errorf("wrong idx, got %d, want %d", i, tt.idx)
+		}
+	}
+
+}
