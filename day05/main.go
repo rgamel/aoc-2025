d0faae07b48221b84346340fe22af5cf4bb04ec9
Ryan Gamel <ryan.gamel.ext@bayer.com>
2025-12-05 17:42:02 -0600

    day05 complete

--- day05/main.go
+++ day05/main.go
@@ -5,42 +5,161 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
+	ranges, ids := getInputs("./input.txt")
+	run(ids, ranges, "Part A", PartA)
+	run(ids, ranges, "Part B", PartB)
+}
 
-	f, err := os.Open("./input.txt")
+type IdRange struct {
+	Min int
+	Max int
+}
+
+type Set struct {
+	elements map[int]struct{}
+}
+
+func NewSet() *Set {
+	return &Set{
+		elements: make(map[int]struct{}),
+	}
+}
+
+func (s *Set) Add(value int) {
+	s.elements[value] = struct{}{}
+}
+
+func (s *Set) Size() int {
+	return len(s.elements)
+}
+
+func getInputs(filename string) (ranges []IdRange, ids []int) {
+	f, err := os.Open(filename)
 	if err != nil {
 		log.Fatalf("problem reading input file: %v\n", err)
 	}
 	defer f.Close()
 
-	lines := []string{}
+	isIds := false
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		line := scanner.Text()
-		lines = append(lines, line)
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			isIds = true
+		} else {
+			if isIds {
+				id, err := strconv.Atoi(line)
+				if err != nil {
+					log.Fatalf("problem reading id: %v\n", err)
+				}
+				ids = append(ids, id)
+			} else {
+				ranges = append(ranges, parseRanges(line))
+			}
+		}
+
 	}
-
-	run(lines, "Part A", PartA)
-	run(lines, "Part B", PartB)
-
+	return ranges, ids
 }
 
-func run(lines []string, label string, cb func(lines []string) int) {
+func parseRanges(s string) IdRange {
+	sl := strings.Split(s, "-")
+
+	minVal, err := strconv.Atoi(sl[0])
+	if err != nil {
+		log.Fatalf("problem parsing range minimum: %v", err)
+	}
+
+	maxVal, err := strconv.Atoi(sl[1])
+	if err != nil {
+		log.Fatalf("problem parsing range maximum: %v", err)
+	}
+
+	return IdRange{Min: minVal, Max: maxVal}
+}
+
+func run(ids []int, ranges []IdRange, label string, cb func(ids []int, ranges []IdRange) int) {
 	start := time.Now()
-	res := cb(lines)
+	res := cb(ids, ranges)
 	end := time.Now()
 	diff := end.Sub(start)
 	fmt.Printf("%s: %d, duration: %d\n", label, res, diff.Microseconds())
 }
 
-func PartA(lines []string) int {
+func PartA(ids []int, ranges []IdRange) int {
+	fresh := NewSet()
+	for _, id := range ids {
+		for _, idRange := range ranges {
+			if IsInRange(idRange, id) {
+				fresh.Add(id)
+
+			}
+		}
+	}
+	return fresh.Size()
+}
+
+func sortByMin(a IdRange, b IdRange) int {
+	if a.Min < b.Min {
+		return -1
+	}
+
+	if a.Min > b.Min {
+		return 1
+	}
 	return 0
 }
 
-func PartB(lines []string) int {
-	return 0
+func PartB(ids []int, ranges []IdRange) int {
+	// sort ranges by min
+	slices.SortFunc(ranges, sortByMin)
+	// merge ranges when max of next range is inside current range
+	merged := mergeSortedRanges(ranges)
+
+	total := 0
+	for _, r := range merged {
+		// do max - min + 1
+		total += r.Max - r.Min + 1
+	}
+	return total
+}
+
+func IsInRange(idRange IdRange, id int) bool {
+	return id >= idRange.Min && id <= idRange.Max
+}
+
+func mergeSortedRanges(ranges []IdRange) []IdRange {
+	for {
+		count := len(ranges)
+		for i := 0; i < len(ranges)-1; i++ {
+			for j := i + 1; j < len(ranges); j++ {
+				if IsInRange(ranges[i], ranges[j].Min) && IsInRange(ranges[i], ranges[j].Max) {
+					// j is completely within i
+					// delete it
+					ranges = deleteFromListAt(ranges, j)
+				} else if IsInRange(ranges[i], ranges[j].Min) && !IsInRange(ranges[i], ranges[j].Max) {
+					// overlap with j starting within i and terminating higher than i
+					// max i's max equal j's, then delete it
+					ranges[i].Max = ranges[j].Max
+					ranges = deleteFromListAt(ranges, j)
+				}
+			}
+		}
+		if count == len(ranges) {
+			break
+		}
+	}
+	return ranges
+}
+
+func deleteFromListAt(list []IdRange, idx int) []IdRange {
+	return append(list[:idx], list[idx+1:]...)
 }
--- day05/main_test.go
+++ day05/main_test.go
@@ -1,34 +1,69 @@
 package main
 
 import (
-	"bufio"
-	"log"
-	"os"
+	"slices"
 	"testing"
 )
 
-func getTestInput() []string {
-	f, err := os.Open("./testinput.txt")
-	if err != nil {
-		log.Fatalf("problem opening test file: %v", err)
-	}
-	defer f.Close()
-	scanner := bufio.NewScanner(f)
-	matrix := []string{}
-	for scanner.Scan() {
-		matrix = append(matrix, scanner.Text())
-	}
-	return matrix
-}
-
 func TestPartA(t *testing.T) {
-	// input := getTestInput()
-	// res := PartA(input)
-	t.Errorf("not implemented")
+	ranges, ids := getInputs("./testinput.txt")
+	res := PartA(ids, ranges)
+
+	if res != 3 {
+		t.Errorf("Got %d want 3\n", res)
+	}
+
 }
 
 func TestPartB(t *testing.T) {
-	// input := getTestInput()
-	// res := PartB(input)
-	t.Errorf("not implemented")
+	ranges, ids := getInputs("./testinput.txt")
+	res := PartB(ids, ranges)
+
+	if res != 14 {
+		t.Errorf("Got %d want 14\n", res)
+	}
+}
+
+func TestIsInRange(t *testing.T) {
+	tests := []struct {
+		rangeStr IdRange
+		id       int
+		out      bool
+	}{
+		{IdRange{Min: 12340, Max: 12349}, 12340, true},
+		{IdRange{Min: 12340, Max: 12349}, 12349, true},
+		{IdRange{Min: 12340, Max: 12349}, 12339, false},
+		{IdRange{Min: 12340, Max: 12349}, 12350, false},
+	}
+
+	for _, tt := range tests {
+		res := IsInRange(tt.rangeStr, tt.id)
+		if tt.out != res {
+			t.Errorf("got %v, want %v", res, tt.out)
+		}
+	}
+}
+
+func TestMergeSortedRanges(t *testing.T) {
+	tests := []struct {
+		ranges []IdRange
+		out    int
+	}{
+		{ranges: []IdRange{
+			{Min: 5, Max: 11},
+			{Min: 1, Max: 10},
+			{Min: 12, Max: 15},
+		}, out: 2},
+	}
+
+	for _, tt := range tests {
+		slices.SortFunc(tt.ranges, sortByMin)
+		res := mergeSortedRanges(tt.ranges)
+		t.Log(res)
+
+		if len(res) != tt.out {
+			t.Errorf("wanted %d, got %d\n", tt.out, len(res))
+		}
+	}
+
 }
